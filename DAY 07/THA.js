06c767c878e54d88836cf47a2591306cc5c0a63b
noob-anmol <anmolghai50@gmail.com>
2021-06-13 23:31:25 +0530

    Added DAY 07

--- DAY 07/THA.js
+++ DAY 07/THA.js
@@ -1,6 +1,6 @@
 //Solution to Question 01
 
-function listProperties(objectHere) {
+function addListProperties(objectHere) {
   let x = "";
   for (i in objectHere) {
     x += i + ",";
@@ -11,13 +11,14 @@ function listProperties(objectHere) {
   */
 }
 
-console.log(listProperties({ name: "David Rayy", sclass: "VI", rollno: 12 }));
+console.log(
+  addListProperties({ name: "David Rayy", sclass: "VI", rollno: 12 })
+);
 
 //Solution to Question 02
 
 const deleteProperty = (objectHere, deleteThis) => {
-  delete objectHere.deleteThis;
-  delete objectHere.rollno;
+  delete objectHere[deleteThis];
   return objectHere;
 };
 
@@ -34,3 +35,23 @@ function lengthOfObject(objectHere) {
 console.log(lengthOfObject({ name: "David Rayy", sclass: "VI", rollno: 12 }));
 
 //Solution to Question 04
+
+function display(list) {
+  for (i of list) {
+    console.log(i);
+  }
+}
+
+var library = [
+  { author: "Bill Gates", title: "The Road Ahead", readingStatus: true },
+  { author: "Steve Jobs", title: "Walter Isaacson", readingStatus: true },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+//Solution to Question 05
+
+function volume(height, radius) {}
